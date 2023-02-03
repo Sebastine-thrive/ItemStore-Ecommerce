efffb73788cd489297e714ef24db2555b31a3e32
Sebastine Ogu (Thrive) <sebastinoptimum@gmail.com>
2023-02-03 04:14:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { configureStore } from '@reduxjs/toolkit';
 import { Provider as ReduxProvider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { persistor, store } from "./store"
-
-// import PropTypes from "prop-types";
 import { createStore } from 'redux';
-
-
-
 import { Provider } from 'react-redux';
-
-// import {cartReducer}  from '../src/reducers/CartReducer'
-
-// const store = createStore(cartReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

a1030b435bcff83daf0c3cdc2603d1664cce804b
Sebastine Ogu (Thrive) <sebastinoptimum@gmail.com>
2023-02-03 04:11:53 +0100

    Update ProductItem.jsx

--- src/components/productDisplay/ProductItem.jsx
+++ src/components/productDisplay/ProductItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { addToCart } from "../../store/CartSlice";
+import { addToCart } from "../../Store/CartSlice";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Link } from 'react-router-dom';
